c4b5abaab84f7f2923eacfd6a00c178d072cd0af
Adriano Vinícius Bispo da Silva <drianvini10@gmail.com>
2023-02-19 15:51:43 -0300

    Create tabuada.php

---
+++ desafios/tabuada.php
@@ -0,0 +1,8 @@
+<?php
+
+$numero = 2;
+
+for ($multiplicador = 1; $multiplicador <= 10; $multiplicador++ ) {
+    $resultado = $numero * $multiplicador;
+    echo "$numero x $multiplicador = $resultado" . PHP_EOL;
+}
