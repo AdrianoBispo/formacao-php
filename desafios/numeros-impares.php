2b4073fdf8a47298c9a9a127f36e9fd33b2f32e2
Adriano Vinícius Bispo da Silva <drianvini10@gmail.com>
2023-02-18 23:45:46 -0300

    Update numeros-impares.php

--- desafios/numeros-impares.php
+++ desafios/numeros-impares.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 $numero = 3;
 $restoDoNumero = $numero % 2;
 
@@ -7,4 +8,11 @@ if ($restoDoNumero == 0 ) {
     echo "Este número é par.";
 } else {
     echo "Este número é ímpar.";
+}
+*/
+
+for ($contador = 1; $contador < 100; $contador++) {
+    if ($contador % 2 != 0) {
+        echo $contador . PHP_EOL;
+    }
 }
