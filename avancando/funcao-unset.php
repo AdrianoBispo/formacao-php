ff079a2a7fb27ac684bde426adb2e244a6c5c346
Adriano Vinícius Bispo da Silva <drianvini10@gmail.com>
2023-02-21 11:21:33 -0300

    Add files via upload

---
+++ avancando/funcao-unset.php
@@ -0,0 +1,10 @@
+<?php
+
+// Como excluir as variáveis $variavel1 e $variavel2 utilizando unset?
+
+// Podemos utiliza-lo assim, de maneira separada.
+unset($variavel1);
+unset($variavel2);
+
+// Ou assim, separando por vírgula dentro de um parâmetro da função.
+unset($variavel1, $variavel2);
