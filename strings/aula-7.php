16eb43f531bb39348939e5492fdd6273b3b3c0ef
AdrianoBispo <Drianvini10@gmail.com>
2023-02-22 23:23:32 -0300

    :heavy_plus_sign: add: Adding the file 'aula-8'

--- strings/aula-7.php
+++ strings/aula-7.php
@@ -4,9 +4,13 @@
 // segundo parâmetro é a string que quero separar
 
 $nome = 'Adriano Vinícius Bispo da Silva';
-var_dump(explode(' ', $nome)); // Separa strings retornando uma array
+var_dump(explode(' ', $nome));
 
 
 // Também é possível utilizar separar strings de vírgulas
 $csv = 'Adriano Bispo, 18, estudante';
 var_dump(explode(',', $csv));
+
+// Essa função também possui um terceiro parâmetro, onde podemos informar o limite
+$array = '1 2 3 4';
+var_dump(explode(' ', $array));
