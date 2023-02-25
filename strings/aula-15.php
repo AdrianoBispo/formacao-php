f0484fda596764ef7a90b6238694697dea28a562
AdrianoBispo <Drianvini10@gmail.com>
2023-02-24 22:06:10 -0300

    :heavy_plus_sign: add: Adding the file 'aula-15'

--- strings/aula-15.php
+++ strings/aula-15.php
@@ -20,3 +20,21 @@ foreach ($telefone as $telefone) {
         echo 'Telefone Inválido';
     }
 }
+
+// Atividade - Verificação de CPF
+
+$cpf = ['123.456.789-10', '123.456.789-11', '123.456.789-12'];
+
+foreach ($cpf as $cpf) {
+    $validandoCPF = preg_match(
+        '/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}$/',
+        $cpf,
+        $cpfCapturados
+    )
+}
+
+if ($validandoCPF) {
+    echo 'CPF Válido';
+} else {
+    echo 'CPF inválido';
+}
