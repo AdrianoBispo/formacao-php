c29ac7fe9a674b2e05805d2287b7dd46f2b6f331
AdrianoBispo <Drianvini10@gmail.com>
2023-02-24 22:57:47 -0300

    :heavy_plus_sign: add: Adding the file 'aula-16'

---
+++ strings/aula-16.php
@@ -0,0 +1,29 @@
+<?php
+
+// Regex (Regular Expression) 
+// Acesse: https://regex101.com/
+
+$telefone = ['(24) 99999 - 9999', '(21) 99999 - 9999', '(24) 2222 - 2222'];
+
+foreach ($telefone as $telefone) {
+
+    $regex = '/^\(([0-9]{2})\) (9?[0-9]{4} - [0-9]{4})$/';
+    $telefoneValido = preg_match(
+        $regex,
+        $telefone, // Variável para fazer o "match" com nossa Regex
+        $correspondencia // Variável que armazena os valores interpretados pela operação de "match"
+    );
+    var_dump($correspondencia); // Retorna os valores capturados da array $telefones
+
+    if ($telefoneValido) {
+        echo 'Telefone Válido' . PHP_EOL;
+    } else {
+        echo 'Telefone Inválido';
+    }
+
+    echo preg_replace( // Substituição utilizando Regex
+        $regex,
+        '(XX) \2',
+        $telefone
+    );
+}
