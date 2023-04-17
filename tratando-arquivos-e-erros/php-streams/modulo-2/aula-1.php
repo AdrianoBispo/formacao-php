ef4318295348d5cf49d8b1145bc430d43ecee6cb
AdrianoBispo <Drianvini10@gmail.com>
2023-04-17 16:14:24 -0300

    :sparkles: Updated files

--- tratando-arquivos-e-erros/php-streams/modulo-2/aula-1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$arquivo = fopen('cursos-php.txt', 'w');
-
-$curso = 'Design Patterns PHP I: Boas práticas de programação';
-
-fwrite($arquivo, $curso);
-
-fclose($arquivo);
--- tratando-arquivos-e-erros/php-streams/modulo-2/aula-3.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$curso = "Design Patterns PHP I: Boas práticas de programação";
-
-file_put_contents('cursos-php.txt', $curso);
